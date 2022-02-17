1e859f6bf593742b34ab8103287c23ac8c5e5fa9
ckleinschmidt <ckleinschmidt@unicef.org>
2022-02-17 08:41:06 -0500

    Use email from ad

--- easy_auth/backends.py
+++ easy_auth/backends.py
@@ -4,10 +4,9 @@ from django.contrib.auth.models import User
 
 class EasyAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None):
-        user_name = request.headers.get('X-MS-CLIENT-PRINCIPAL-NAME')
         user_id = request.headers.get('X-MS-CLIENT-PRINCIPAL-ID')
         try:
-            user = User.objects.get(username=user_name)
+            user = User.objects.get(email=user_id)
         except User.DoesNotExist:
             return None
         return user
