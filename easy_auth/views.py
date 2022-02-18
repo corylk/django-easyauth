0c8ce9273883506ab67d617fae9728451aeecc18
ckleinschmidt <ckleinschmidt@unicef.org>
2022-02-18 07:49:48 -0500

    Return to loginview

--- easy_auth/views.py
+++ easy_auth/views.py
@@ -2,24 +2,21 @@ from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView
 from django.http.response import HttpResponseRedirect
-from django.views.generic.base import RedirectView
 
 
-class EasyAuthLoginView(RedirectView):
-    pattern_name = 'next'
+class EasyAuthLoginView(LoginView):
+    template_name = 'admin/login.html'
 
     def dispatch(self, request, *args, **kwargs):
         user = authenticate(request)
         if not user:
-            return LoginView.as_view(template_name=getattr(
-                settings, 'LOGIN_TEMPLATE', 'admin/login.html'))(request)
+            return LoginView.as_view(template_name=self.template_name)(request)
 
         login(request, user)
-        redirect_to = self.get_redirect_url(*args, **kwargs)
+        redirect_to = self.get_success_url()
         if redirect_to == self.request.path:
             raise ValueError(
                 "Redirection loop for authenticated user detected. Check that "
                 "your LOGIN_REDIRECT_URL doesn't point to a login page."
             )
-
         return HttpResponseRedirect(redirect_to)
