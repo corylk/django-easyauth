8230c766edd1964338573fba11e0234d55fb3311
ckleinschmidt <ckleinschmidt@unicef.org>
2022-02-19 08:06:15 -0500

    Rename some vars

--- easy_auth/constants.py
+++ easy_auth/constants.py
@@ -1,4 +1,4 @@
-OPTIONS = {
+DEFAULTS = {
     'LOGIN_TEMPLATE': 'admin/login.html',
     'REDIRECT_AUTH_USERS': False,
     'USERID_HEADER': 'x-ms-client-principal-name',
--- easy_auth/helpers.py
+++ easy_auth/helpers.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 import logging
 
-from .options import OPTIONS
+from .constants import DEFAULTS
 
 logger = logging.getLogger(__name__)
 
 
 def get_option(key):
-    return getattr(settings, key, OPTIONS.get(key))
+    return getattr(settings, key, DEFAULTS.get(key))
 
 def get_aad_user_id(headers):
     user_id = headers.get(get_option('USERID_HEADER'))
